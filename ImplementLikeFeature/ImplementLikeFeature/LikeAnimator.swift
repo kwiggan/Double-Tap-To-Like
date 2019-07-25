f1c8a47543670c385267f123bf5aad2e5db9bf45
Keneisha Wiggan <keneishawiggan8@gmail.com>
2019-07-25 16:30:53 -0700

    Add like animator swift file with like icon

---
+++ ImplementLikeFeature/ImplementLikeFeature/LikeAnimator.swift
@@ -0,0 +1,30 @@
+//
+//  LikeAnimator.swift
+//  ImplementLikeFeature
+//
+//  Created by Keneisha Wiggan on 7/25/19.
+//  Copyright © 2019 Keneisha Wiggan. All rights reserved.
+//
+
+import UIKit
+
+class LikeAnimator: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
